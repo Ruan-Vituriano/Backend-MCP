27f8700be4c1493b7209e223d9466396d158c572
Ruan Vituriano <ruanviturianoc@gmail.com>
2026-05-04 20:42:14 -0300

    feat: libera rotas http

--- app/main.py
+++ app/main.py
@@ -3,6 +3,14 @@ from app.routes.chat_route import router as chat_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_credentials=True,
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def root():
     return {"status": "ok"}
