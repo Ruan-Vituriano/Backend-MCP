211e4c7277de82c09e4b25fefb6efbc99ec3d431
Ruan Vituriano <ruanviturianoc@gmail.com>
2026-05-06 08:37:36 -0300

    feat: melhora as tools calling

--- app/ai/agent.py
+++ app/ai/agent.py
@@ -2,7 +2,13 @@ from google import genai
 from app.core.config import GEMINI_API_KEY
 from app.ai.rag.rag_service import buscar_contexto
 from app.ai.rag.rag_loader import carregar_documentos, criar_base
-from app.ai.mcp.tools import clima_atual, recomendacao_cultura
+from app.ai.mcp.tools import (
+    clima_atual,
+    recomendacao_cultura,
+    diagnostico_sintomas,
+    controle_pragas,
+    calculo_adubacao
+)
 from app.ai.mcp.executor import executar_tool
 
 client = genai.Client(api_key=GEMINI_API_KEY)
@@ -18,10 +24,6 @@ def _get_base_rag():
 def gerar_resposta(pergunta: str):
     base = _get_base_rag()
     contexto = buscar_contexto(pergunta, base)
-    if contexto:
-        print("[RAG] Contexto encontrado:\n" + contexto)
-    else:
-        print("[RAG] Nenhum contexto encontrado.")
 
     prompt = f"""
     Você é um assistente agrícola.
@@ -40,7 +42,13 @@ def gerar_resposta(pergunta: str):
         model="gemini-2.5-flash",
         contents=prompt,
         config={
-            "tools": [clima_atual, recomendacao_cultura]
+            "tools": [
+                clima_atual,
+                recomendacao_cultura,
+                diagnostico_sintomas,
+                controle_pragas,
+                calculo_adubacao
+            ]
         }
     )
     
--- app/ai/mcp/executor.py
+++ app/ai/mcp/executor.py
@@ -1,4 +1,10 @@
-from app.ai.mcp.tools import clima_atual, recomendacao_cultura
+from app.ai.mcp.tools import (
+    clima_atual,
+    recomendacao_cultura,
+    diagnostico_sintomas,
+    controle_pragas,
+    calculo_adubacao
+)
 
 def executar_tool(nome: str, args: dict):
     if nome == "clima_atual":
@@ -6,5 +12,14 @@ def executar_tool(nome: str, args: dict):
 
     elif nome == "recomendacao_cultura":
         return recomendacao_cultura(**args)
+
+    elif nome == "diagnostico_sintomas":
+        return diagnostico_sintomas(**args)
+
+    elif nome == "controle_pragas":
+        return controle_pragas(**args)
+
+    elif nome == "calculo_adubacao":
+        return calculo_adubacao(**args)
 
     return "Tool não encontrada"
